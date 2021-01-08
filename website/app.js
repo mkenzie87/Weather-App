a67d8399ca04c854694c3dcf3c328091444c8e96
Matthew Mckenzie <matthewmckenzie@Matthews-MacBook-Pro.local>
2021-01-07 22:31:59 -0600

    Starting project files add cors and epress

--- website/app.js
+++ website/app.js
@@ -1,5 +1,11 @@
 /* Global Variables */
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+// Personal API Key for OpenWeatherMap API
+
+// api url = api.openweathermap.org/data/2.5/forecast?zip={zip code},{country code}&appid={API key}
+let baseURL = 'api.openweathermap.org/data/2.5/forecast?zip=';
+const apiKey = '6c413c6e3c5f59807918f3a330cbc154';
+
+const zip = document.getElementById('zip').value;
+
+// Event listener to add function to existing HTML DOM element
